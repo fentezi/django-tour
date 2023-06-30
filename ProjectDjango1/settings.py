e8d6e06d423952d74182a165c7582b2e35284e31
fentezi <serouhn@gmail.com>
2023-06-30 14:49:04 +0300

    sett

--- ProjectDjango1/settings.py
+++ ProjectDjango1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wdv4zgbx$!rw62lqx2y9==%_492&w*2@5-=5yb=sb2ip-kj0b5'
+env = Env()
+env.read_env()
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
